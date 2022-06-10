27529f6a89a0333a9f11b471afef1f7d22bf01b0
bmlewandowski <bmlewandowski@gmail.com>
2022-06-10 04:39:03 -0400

    minor housekeeping

--- src/app/modules/admin/administration/admin-reports/admin-reports.component.ts
+++ src/app/modules/admin/administration/admin-reports/admin-reports.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-admin-reports',
   templateUrl: './admin-reports.component.html',
-  styleUrls: ['./admin-reports.component.scss']
+  styleUrls: ['./admin-reports.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AdminReportsComponent implements OnInit {
 
