728202381ad04f436ebeb77a1aecfec5fdae13e5
bmlewandowski <bmlewandowski@gmail.com>
2022-06-14 05:47:05 -0400

    changed degreelevel json

--- src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
+++ src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
@@ -41,12 +41,13 @@ export class AcademicDegreesComponent implements OnInit {
   degreetypesfilteredData;
   states = [{ "name": "Alabama", "abbreviation": "AL" }, { "name": "Alaska", "abbreviation": "AK" }, { "name": "Arizona", "abbreviation": "AZ" }, { "name": "Arkansas", "abbreviation": "AR" }, { "name": "California", "abbreviation": "CA" }, { "name": "Colorado", "abbreviation": "CO" }, { "name": "Connecticut", "abbreviation": "CT" }, { "name": "Delaware", "abbreviation": "DE" }, { "name": "Florida", "abbreviation": "FL" }, { "name": "Georgia", "abbreviation": "GA" }, { "name": "Hawaii", "abbreviation": "HI" }, { "name": "Idaho", "abbreviation": "ID" }, { "name": "Illinois", "abbreviation": "IL" }, { "name": "Indiana", "abbreviation": "IN" }, { "name": "Iowa", "abbreviation": "IA" }, { "name": "Kansas", "abbreviation": "KS" }, { "name": "Kentucky", "abbreviation": "KY" }, { "name": "Louisiana", "abbreviation": "LA" }, { "name": "Maine", "abbreviation": "ME" }, { "name": "Maryland", "abbreviation": "MD" }, { "name": "Massachusetts", "abbreviation": "MA" }, { "name": "Michigan", "abbreviation": "MI" }, { "name": "Minnesota", "abbreviation": "MN" }, { "name": "Mississippi", "abbreviation": "MS" }, { "name": "Missouri", "abbreviation": "MO" }, { "name": "Montana", "abbreviation": "MT" }, { "name": "Nebraska", "abbreviation": "NE" }, { "name": "Nevada", "abbreviation": "NV" }, { "name": "New Hampshire", "abbreviation": "NH" }, { "name": "New Jersey", "abbreviation": "NJ" }, { "name": "New Mexico", "abbreviation": "NM" }, { "name": "New York", "abbreviation": "NY" }, { "name": "North Carolina", "abbreviation": "NC" }, { "name": "North Dakota", "abbreviation": "ND" }, { "name": "Ohio", "abbreviation": "OH" }, { "name": "Oklahoma", "abbreviation": "OK" }, { "name": "Oregon", "abbreviation": "OR" }, { "name": "Pennsylvania", "abbreviation": "PA" }, { "name": "Rhode Island", "abbreviation": "RI" }, { "name": "South Carolina", "abbreviation": "SC" }, { "name": "South Dakota", "abbreviation": "SD" }, { "name": "Tennessee", "abbreviation": "TN" }, { "name": "Texas", "abbreviation": "TX" }, { "name": "Utah", "abbreviation": "UT" }, { "name": "Vermont", "abbreviation": "VT" }, { "name": "Virginia", "abbreviation": "VA" }, { "name": "Washington", "abbreviation": "WA" }, { "name": "West Virginia", "abbreviation": "WV" }, { "name": "Wisconsin", "abbreviation": "WI" }, { "name": "Wyoming", "abbreviation": "WY" }];
 
+  // degreelevels;
   degreelevels =
     [
-      { id: '1', name: 'Associate' },
-      { id: '2', name: 'Bachelors' },
-      { id: '3', name: 'Masters' },
-      { id: '4', name: 'Doctorate' }
+      { level: '1', name: 'Associate' },
+      { level: '2', name: 'Bachelors' },
+      { level: '3', name: 'Masters' },
+      { level: '4', name: 'Doctorate' }
     ];
 
   degreetypes =
@@ -161,12 +162,10 @@ export class AcademicDegreesComponent implements OnInit {
     this.schoolfilteredData = this.schooloptions;
     this.degreetypesfilteredData = this.degreetypes;
 
-    // this.degreelevels = this.db.object('/degreelevels').snapshotChanges();
-    // this.degreelevels.subscribe(level => console.log(level));
-
-    // this.degreelevels = this.db.object('/degreelevels').valueChanges()
+    // //Fill dropdown
+    // this.db.object('/degreelevels').valueChanges()
     //   .subscribe((inp) => {
-    //     this.degreelevels = inp;
+    //     this.degreelevels = Object.values(inp);
     //     console.log(this.degreelevels);
     //   });
 
