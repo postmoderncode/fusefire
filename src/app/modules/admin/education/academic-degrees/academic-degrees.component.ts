94f892f3e8fb239955700fb26b7c5836bafda6dc
bmlewandowski <bmlewandowski@gmail.com>
2022-06-14 06:09:20 -0400

    angularfire dropdowns

--- src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
+++ src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
@@ -41,14 +41,16 @@ export class AcademicDegreesComponent implements OnInit {
   degreetypesfilteredData;
   states = [{ "name": "Alabama", "abbreviation": "AL" }, { "name": "Alaska", "abbreviation": "AK" }, { "name": "Arizona", "abbreviation": "AZ" }, { "name": "Arkansas", "abbreviation": "AR" }, { "name": "California", "abbreviation": "CA" }, { "name": "Colorado", "abbreviation": "CO" }, { "name": "Connecticut", "abbreviation": "CT" }, { "name": "Delaware", "abbreviation": "DE" }, { "name": "Florida", "abbreviation": "FL" }, { "name": "Georgia", "abbreviation": "GA" }, { "name": "Hawaii", "abbreviation": "HI" }, { "name": "Idaho", "abbreviation": "ID" }, { "name": "Illinois", "abbreviation": "IL" }, { "name": "Indiana", "abbreviation": "IN" }, { "name": "Iowa", "abbreviation": "IA" }, { "name": "Kansas", "abbreviation": "KS" }, { "name": "Kentucky", "abbreviation": "KY" }, { "name": "Louisiana", "abbreviation": "LA" }, { "name": "Maine", "abbreviation": "ME" }, { "name": "Maryland", "abbreviation": "MD" }, { "name": "Massachusetts", "abbreviation": "MA" }, { "name": "Michigan", "abbreviation": "MI" }, { "name": "Minnesota", "abbreviation": "MN" }, { "name": "Mississippi", "abbreviation": "MS" }, { "name": "Missouri", "abbreviation": "MO" }, { "name": "Montana", "abbreviation": "MT" }, { "name": "Nebraska", "abbreviation": "NE" }, { "name": "Nevada", "abbreviation": "NV" }, { "name": "New Hampshire", "abbreviation": "NH" }, { "name": "New Jersey", "abbreviation": "NJ" }, { "name": "New Mexico", "abbreviation": "NM" }, { "name": "New York", "abbreviation": "NY" }, { "name": "North Carolina", "abbreviation": "NC" }, { "name": "North Dakota", "abbreviation": "ND" }, { "name": "Ohio", "abbreviation": "OH" }, { "name": "Oklahoma", "abbreviation": "OK" }, { "name": "Oregon", "abbreviation": "OR" }, { "name": "Pennsylvania", "abbreviation": "PA" }, { "name": "Rhode Island", "abbreviation": "RI" }, { "name": "South Carolina", "abbreviation": "SC" }, { "name": "South Dakota", "abbreviation": "SD" }, { "name": "Tennessee", "abbreviation": "TN" }, { "name": "Texas", "abbreviation": "TX" }, { "name": "Utah", "abbreviation": "UT" }, { "name": "Vermont", "abbreviation": "VT" }, { "name": "Virginia", "abbreviation": "VA" }, { "name": "Washington", "abbreviation": "WA" }, { "name": "West Virginia", "abbreviation": "WV" }, { "name": "Wisconsin", "abbreviation": "WI" }, { "name": "Wyoming", "abbreviation": "WY" }];
 
-  // degreelevels;
-  degreelevels =
-    [
-      { level: '1', name: 'Associate' },
-      { level: '2', name: 'Bachelors' },
-      { level: '3', name: 'Masters' },
-      { level: '4', name: 'Doctorate' }
-    ];
+  degreelevels;
+  // degreelevels =
+  //   [
+  //     { level: '1', name: 'Associate' },
+  //     { level: '2', name: 'Bachelors' },
+  //     { level: '3', name: 'Masters' },
+  //     { level: '4', name: 'Doctorate' }
+  //   ];
+
+  //degreetypes;
 
   degreetypes =
     [
@@ -151,24 +153,29 @@ export class AcademicDegreesComponent implements OnInit {
   //Change fileter of degree types based on degree level dropdown
   onDegreeLevelChanged(ob): void {
 
-    this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel === ob.value);
+    this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel === ob.value.toString());
 
   }
 
 
   ngOnInit(): void {
 
+    //Fill Degree Levels dropdown
+    this.db.object('/degreelevels').valueChanges()
+      .subscribe((inp) => {
+        this.degreelevels = Object.values(inp);
+      }).unsubscribe;
+
+    // //Fill Degree Types dropdown
+    // this.db.object('/degreetypes').valueChanges()
+    //   .subscribe((inp) => {
+    //     this.degreetypes = Object.values(inp);
+    //   });
+
     this.fieldfilteredData = this.fieldoptions;
     this.schoolfilteredData = this.schooloptions;
     this.degreetypesfilteredData = this.degreetypes;
-
-    // //Fill dropdown
-    // this.db.object('/degreelevels').valueChanges()
-    //   .subscribe((inp) => {
-    //     this.degreelevels = Object.values(inp);
-    //     console.log(this.degreelevels);
-    //   });
-
+    console.log(this.degreetypesfilteredData);
 
   }
 
