0fd8cfad407bb4b25a88f8c02406a4e3b5d2efd4
bmlewandowski <bmlewandowski@gmail.com>
2022-06-14 02:43:21 -0400

    Typescript fixes

--- src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
+++ src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
@@ -23,7 +23,7 @@ export class AcademicDegreesComponent implements OnInit {
   dialogconfigForm: FormGroup;
 
   //Empty Model
-  model = new Education('', '', '', '', '', '', '', '', '');
+  model = new Education('', '', '', '', '', '', '', '', '', '');
 
   //Form Visibility Modifiers
   showadditem = false;
@@ -33,39 +33,39 @@ export class AcademicDegreesComponent implements OnInit {
   schoolfilteredData;
   fieldfilteredData;
 
-  schooloptions: string[] = ["Ball State University", "Indiana University", "Notre Dame", "Purdue University", "Indiana Tech"];
-  fieldoptions: string[] = ["Accounting", "Advertising", "African-American Studies", "Agriculture", "Animal Science", "Anthropology", "Aerospace Engineering", "Archaeology", "Architecture", "Art History", "Arts Management", "Asian-American Studies", "Astronomy and Astrophysics", "Bilingual/Crosscultural Education", "Athletic Training", "Biochemistry", "Biology", "Biomedical Engineering", "Business", "Chemical Engineering", "Chemistry", "Civil Engineering", "Classical Studies", "Communication Disorders Sciences and Services", "Communications", "Comparative Literature", "Computer Engineering", "Computer Information Systems", "Computer Science", "Construction Services", "Cosmetology Services", "Creative Writing", "Criminology", "Culinary Arts", "Cybersecurity", "Design", "Economics", "Education", "Electrical Engineering", "Elementary Education", "Engineering", "English Language and Literature", "Entomology", "Environmental Engineering", "Film and Video Production", "Film-Video Arts", "Finance", "Fine Arts", "Fire Safety Science", "Food Science", "Foreign Languages", "Forestry", "Gender Studies", "Genetics", "Geology", "Graphic Design", "Health Sciences", "History", "Hospitality Management", "Human Ecology", "Industrial Technology", "International Business", "International Relations", "Journalism", "Kinesiology", "Latin American Studies", "Liberal Studies", "Library Science", "Linguistics", "Logistics Management", "Marketing", "Mathematics", "Mechanical Engineering", "Medical Technology", "Metallurgical Engineering", "Meteorology", "Microbiology", "Military Technology", "Mining and Mineral Engineering", "Music", "Mythology and Folklore", "Naval Architecture and Marine Engineering", "Neuroscience", "Nuclear Engineering", "Nursing", "Oceanography", "Occupational Health and Safety", "Parks, Recreation, and Leisure Studies", "Performing Arts", "Petroleum Engineering", "Pharmacology", "Philosophy", "Photography", "Physics", "Physiology", "Plant Science", "Political Science", "Pre-Law", "Psychology", "Public Administration", "Puppetry", "Religious Studies", "Rhetoric", "Social Work", "Sociology", "Software Engineering", "Special Education", "Sports Medicine", "Statistics", "Student Counseling", "Supply Chain Management", "Theater Arts", "Viticulture", "Zoology"]
+  schooloptions: string[] = ['Ball State University', 'Indiana University', 'Notre Dame', 'Purdue University', 'Indiana Tech'];
+  fieldoptions: string[] = ['Accounting', 'Advertising', 'African-American Studies', 'Agriculture', 'Animal Science', 'Anthropology', 'Aerospace Engineering', 'Archaeology', 'Architecture', 'Art History', 'Arts Management', 'Asian-American Studies', 'Astronomy and Astrophysics', 'Bilingual/Crosscultural Education', 'Athletic Training', 'Biochemistry', 'Biology', 'Biomedical Engineering', 'Business', 'Chemical Engineering', 'Chemistry', 'Civil Engineering', 'Classical Studies', 'Communication Disorders Sciences and Services', 'Communications', 'Comparative Literature', 'Computer Engineering', 'Computer Information Systems', 'Computer Science', 'Construction Services', 'Cosmetology Services', 'Creative Writing', 'Criminology', 'Culinary Arts', 'Cybersecurity', 'Design', 'Economics', 'Education', 'Electrical Engineering', 'Elementary Education', 'Engineering', 'English Language and Literature', 'Entomology', 'Environmental Engineering', 'Film and Video Production', 'Film-Video Arts', 'Finance', 'Fine Arts', 'Fire Safety Science', 'Food Science', 'Foreign Languages', 'Forestry', 'Gender Studies', 'Genetics', 'Geology', 'Graphic Design', 'Health Sciences', 'History', 'Hospitality Management', 'Human Ecology', 'Industrial Technology', 'International Business', 'International Relations', 'Journalism', 'Kinesiology', 'Latin American Studies', 'Liberal Studies', 'Library Science', 'Linguistics', 'Logistics Management', 'Marketing', 'Mathematics', 'Mechanical Engineering', 'Medical Technology', 'Metallurgical Engineering', 'Meteorology', 'Microbiology', 'Military Technology', 'Mining and Mineral Engineering', 'Music', 'Mythology and Folklore', 'Naval Architecture and Marine Engineering', 'Neuroscience', 'Nuclear Engineering', 'Nursing', 'Oceanography', 'Occupational Health and Safety', 'Parks, Recreation, and Leisure Studies', 'Performing Arts', 'Petroleum Engineering', 'Pharmacology', 'Philosophy', 'Photography', 'Physics', 'Physiology', 'Plant Science', 'Political Science', 'Pre-Law', 'Psychology', 'Public Administration', 'Puppetry', 'Religious Studies', 'Rhetoric', 'Social Work', 'Sociology', 'Software Engineering', 'Special Education', 'Sports Medicine', 'Statistics', 'Student Counseling', 'Supply Chain Management', 'Theater Arts', 'Viticulture', 'Zoology']
 
   degreelevels =
     [
-      { id: "1", name: "Associate" },
-      { id: "2", name: "Bachelors" },
-      { id: "3", name: "Masters" },
-      { id: "4", name: "Doctorate" }
+      { id: '1', name: 'Associate' },
+      { id: '2', name: 'Bachelors' },
+      { id: '3', name: 'Masters' },
+      { id: '4', name: 'Doctorate' }
     ];
 
   //degreelevels;
 
   degreetypes =
     [
-      { name: "Associate of Arts (AA)", degreeLevel: "1" },
-      { name: "Associate of Science (AS)", degreeLevel: "1" },
-      { name: "Associate of Applied Science (AAS)", degreeLevel: "1" },
-      { name: "Associate of Applied Arts and Science (AAAS)", degreeLevel: "1" },
-      { name: "Bachelor of Arts (B.A.)", degreeLevel: "2" },
-      { name: "Bachelor of Science (B.S.)", degreeLevel: "2" },
-      { name: "Bachelor of Fine Arts (BFA)", degreeLevel: "2" },
-      { name: "Bachelor of Applied Science (BAS)", degreeLevel: "2" },
-      { name: "Bachelor of Applied Arts and Science (BAAS)", degreeLevel: "2" },
-      { name: "Master of Arts (M.A.)", degreeLevel: "3" },
-      { name: "Master of Science (M.S.)", degreeLevel: "3" },
-      { name: "Master of Business Administration (MBA)", degreeLevel: "3" },
-      { name: "Master of Fine Arts (MFA)", degreeLevel: "3" },
-      { name: "Doctor of Philosophy (Ph.D.)", degreeLevel: "4" },
-      { name: "Juris Doctor (J.D.)", degreeLevel: "4" },
-      { name: "Doctor of Medicine (M.D.)", degreeLevel: "4" },
-      { name: "Doctor of Dental Surgery (DDS)", degreeLevel: "4" }
-    ]
+      { name: 'Associate of Arts (AA)', degreeLevel: '1' },
+      { name: 'Associate of Science (AS)', degreeLevel: '1' },
+      { name: 'Associate of Applied Science (AAS)', degreeLevel: '1' },
+      { name: 'Associate of Applied Arts and Science (AAAS)', degreeLevel: '1' },
+      { name: 'Bachelor of Arts (B.A.)', degreeLevel: '2' },
+      { name: 'Bachelor of Science (B.S.)', degreeLevel: '2' },
+      { name: 'Bachelor of Fine Arts (BFA)', degreeLevel: '2' },
+      { name: 'Bachelor of Applied Science (BAS)', degreeLevel: '2' },
+      { name: 'Bachelor of Applied Arts and Science (BAAS)', degreeLevel: '2' },
+      { name: 'Master of Arts (M.A.)', degreeLevel: '3' },
+      { name: 'Master of Science (M.S.)', degreeLevel: '3' },
+      { name: 'Master of Business Administration (MBA)', degreeLevel: '3' },
+      { name: 'Master of Fine Arts (MFA)', degreeLevel: '3' },
+      { name: 'Doctor of Philosophy (Ph.D.)', degreeLevel: '4' },
+      { name: 'Juris Doctor (J.D.)', degreeLevel: '4' },
+      { name: 'Doctor of Medicine (M.D.)', degreeLevel: '4' },
+      { name: 'Doctor of Dental Surgery (DDS)', degreeLevel: '4' }
+    ];
 
   degreetypesfilteredData;
 
@@ -79,20 +79,20 @@ export class AcademicDegreesComponent implements OnInit {
   ) { }
 
   //Filter for Field of Study Autocomplete
-  applyFilterFields(evt: string) {
-    evt = evt + "";
-    if (!evt) this.fieldfilteredData = this.fieldoptions;
+  applyFilterFields(evt: string): void {
+    evt = evt + '';
+    if (!evt) {this.fieldfilteredData = this.fieldoptions;}
     else {
-      this.fieldfilteredData = this.fieldoptions.filter(item => (item + "") === evt || item.toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
+      this.fieldfilteredData = this.fieldoptions.filter(item => (item + '') === evt || item.toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
     }
   }
 
   //Filter for Institution Autocomplete
-  applyFilterSchools(evt: string) {
-    evt = evt + "";
-    if (!evt) this.schoolfilteredData = this.schooloptions;
+  applyFilterSchools(evt: string): void {
+    evt = evt + '';
+    if (!evt) {this.schoolfilteredData = this.schooloptions;}
     else {
-      this.schoolfilteredData = this.schooloptions.filter(item => (item + "") === evt || item.toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
+      this.schoolfilteredData = this.schooloptions.filter(item => (item + '') === evt || item.toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
     }
   }
 
@@ -131,17 +131,17 @@ export class AcademicDegreesComponent implements OnInit {
   //State Dropdown Change Event
   onStateChange(ob): void {
 
-    let selectedState = ob.value;
+    const selectedState = ob.value;
     console.log(selectedState);
 
-    //TODO: Fill School Dropdown 
+    //TODO: Fill School Dropdown
   }
 
 
   //Change fileter of degree types based on degree level dropdown
   onDegreeLevelChanged(ob): void {
 
-    this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel == ob.value)
+    this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel === ob.value);
 
   }
 
@@ -176,9 +176,10 @@ export class Education {
     public created: string,
     public modified: string,
     public user: string,
-    public awardedby: string,
+    public institution: string,
     public awardedon: string,
-    public expireson: string,
+    public major: string,
+    public minor: string,
 
   ) { }
 
