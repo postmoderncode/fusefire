ca16899b11400f16a2a1a7abf49de2f5bc78b703
bmlewandowski <bmlewandowski@gmail.com>
2022-06-14 01:12:55 -0400

    code cleanup

--- src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
+++ src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
@@ -30,8 +30,8 @@ export class AcademicDegreesComponent implements OnInit {
   showedititem = false;
 
   //Autocomplete Data
-  fieldfilteredData;
   schoolfilteredData;
+  fieldfilteredData;
 
   schooloptions: string[] = ["Ball State University", "Indiana University", "Notre Dame", "Purdue University", "Indiana Tech"];
   fieldoptions: string[] = ["Accounting", "Advertising", "African-American Studies", "Agriculture", "Animal Science", "Anthropology", "Aerospace Engineering", "Archaeology", "Architecture", "Art History", "Arts Management", "Asian-American Studies", "Astronomy and Astrophysics", "Bilingual/Crosscultural Education", "Athletic Training", "Biochemistry", "Biology", "Biomedical Engineering", "Business", "Chemical Engineering", "Chemistry", "Civil Engineering", "Classical Studies", "Communication Disorders Sciences and Services", "Communications", "Comparative Literature", "Computer Engineering", "Computer Information Systems", "Computer Science", "Construction Services", "Cosmetology Services", "Creative Writing", "Criminology", "Culinary Arts", "Cybersecurity", "Design", "Economics", "Education", "Electrical Engineering", "Elementary Education", "Engineering", "English Language and Literature", "Entomology", "Environmental Engineering", "Film and Video Production", "Film-Video Arts", "Finance", "Fine Arts", "Fire Safety Science", "Food Science", "Foreign Languages", "Forestry", "Gender Studies", "Genetics", "Geology", "Graphic Design", "Health Sciences", "History", "Hospitality Management", "Human Ecology", "Industrial Technology", "International Business", "International Relations", "Journalism", "Kinesiology", "Latin American Studies", "Liberal Studies", "Library Science", "Linguistics", "Logistics Management", "Marketing", "Mathematics", "Mechanical Engineering", "Medical Technology", "Metallurgical Engineering", "Meteorology", "Microbiology", "Military Technology", "Mining and Mineral Engineering", "Music", "Mythology and Folklore", "Naval Architecture and Marine Engineering", "Neuroscience", "Nuclear Engineering", "Nursing", "Oceanography", "Occupational Health and Safety", "Parks, Recreation, and Leisure Studies", "Performing Arts", "Petroleum Engineering", "Pharmacology", "Philosophy", "Photography", "Physics", "Physiology", "Plant Science", "Political Science", "Pre-Law", "Psychology", "Public Administration", "Puppetry", "Religious Studies", "Rhetoric", "Social Work", "Sociology", "Software Engineering", "Special Education", "Sports Medicine", "Statistics", "Student Counseling", "Supply Chain Management", "Theater Arts", "Viticulture", "Zoology"]
@@ -76,6 +76,7 @@ export class AcademicDegreesComponent implements OnInit {
     public db: AngularFireDatabase
   ) { }
 
+  //Filter for Field of Study Autocomplete
   applyFilterFields(evt: string) {
     evt = evt + "";
     if (!evt) this.fieldfilteredData = this.fieldoptions;
@@ -84,6 +85,7 @@ export class AcademicDegreesComponent implements OnInit {
     }
   }
 
+  //Filter for Institution Autocomplete
   applyFilterSchools(evt: string) {
     evt = evt + "";
     if (!evt) this.schoolfilteredData = this.schooloptions;
@@ -92,31 +94,39 @@ export class AcademicDegreesComponent implements OnInit {
     }
   }
 
+  //Calendar Change Event to set Model
   onDateChange(event: MatDatepickerInputEvent<any>, control: AbstractControl): void {
 
-    this.model.awardedon = ((event.value.valueOf()).toString());
+    //this.model.awardedon = ((event.value.valueOf()).toString());
+
   }
+
+
+  //Form Visibility Modifiers
 
   onShowAddForm(): void {
     this.showedititem = false;
     this.showadditem = true;
+
   }
 
   onHideAddForm(): void {
     this.showadditem = false;
+
   }
 
   onShowEditForm(key): void {
     this.showadditem = false;
     this.showedititem = true;
 
-  };
+  }
 
   onHideEditForm(): void {
     this.showedititem = false;
 
   }
 
+  //State Dropdown Change Event
   onStateChange(ob): void {
 
     let selectedState = ob.value;
@@ -125,6 +135,8 @@ export class AcademicDegreesComponent implements OnInit {
     //TODO: Fill School Dropdown 
   }
 
+
+  //Change fileter of degree types based on degree level dropdown
   onDegreeLevelChanged(ob): void {
 
     this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel == ob.value)
@@ -132,12 +144,6 @@ export class AcademicDegreesComponent implements OnInit {
 
   }
 
-  onDegreeTypeChanged(ob): void {
-
-    let selectedType = ob.value;
-    console.log(selectedType);
-
-  }
 
   ngOnInit(): void {
 
