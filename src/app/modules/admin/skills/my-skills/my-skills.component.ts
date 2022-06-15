0b1e8e752e3397fd7ca9f40813714bb582bb23a7
bmlewandowski <bmlewandowski@gmail.com>
2022-06-15 12:00:11 -0400

    my skills template

--- src/app/modules/admin/skills/my-skills/my-skills.component.ts
+++ src/app/modules/admin/skills/my-skills/my-skills.component.ts
@@ -12,34 +12,35 @@ import _ from 'lodash';
 })
 export class MySkillsComponent implements OnInit {
 
-  fbuserid: string = localStorage.getItem('fbuserid');
-  dialogconfigForm: FormGroup;
-  value = 0;
-
-  dbRef = this.db.database.ref("/skillcatalog/categories/");
-
-  myObserver = {
-    next: (x: number) => console.log('Observer got a next value: ' + x),
-    error: (err: Error) => console.error('Observer got an error: ' + err),
-    complete: () => console.log('Observer got a complete notification'),
-  };
-
   //Initialize Varables
   //-------------------
 
-  //Object to Hold All Areas. 
+  //Current User
+  fbuser = JSON.parse(localStorage.getItem('fbuser'));
+
+  //Confirmation Dialog
+  dialogconfigForm: FormGroup;
+
+  //Empty Model
+  model = new Skill('', '', '', '', '', '');
+  catmodel = new CatalogState('', '', '');
+
+  //Form Visibility Modifiers
+  showadditem = false;
+  showedititem = false;
+
+  //Object to Hold All Areas.
   areas: Observable<any>;
 
-  //Object to Hold Current Category List. 
+  //Object to Hold Current Category List.
   categories: object;
-  categories2: object;
 
-  //Object to Hold Current Skill List. 
-  skills: Observable<any>;
+  //Object to Hold Current Skill List.
+  skills: object;
 
   //General Component Variables
   selectedIndex = 0;
-  tabTitle = "Please Select a Skill Area"
+  tabTitle = 'Area';
 
   /**
    * Constructor
@@ -50,66 +51,163 @@ export class MySkillsComponent implements OnInit {
     public db: AngularFireDatabase
   ) { }
 
-
-
   //Function to Handle the Back Arrow
   goback(): void {
     switch (this.selectedIndex) {
       case 1: {
-        this.tabTitle = "Please Select a Skill Area";
+        console.log('goback 1');
+        this.tabTitle = 'Area';
         this.selectedIndex = 0;
+        this.catmodel.currentCategory = '';
+        this.catmodel.currentSkill = '';
         break;
       }
       case 2: {
-        this.tabTitle = "Now, select a category...";
+        console.log('goback 2');
+        this.tabTitle = 'Category';
         this.selectedIndex = 1;
+        this.catmodel.currentSkill = '';
         break;
       }
     }
   }
 
   //Function to call when an area is selected
-  loadCategories(categoryId) {
+  onAreaSelect(areaId) {
 
-    //Populate Categories
-    this.db.list('/skillcatalog/categories/', ref => ref.orderByChild("area").equalTo(parseInt(categoryId))).snapshotChanges().subscribe(
-      (results: object) => {
-        console.log(results);
-        this.categories = results;
-      }
-    );
+    //Populate Categories - Firebase List w/ Sort&Filter Query
+    this.db.list('/skillcatalog/categories/', ref => ref
+      .orderByChild("area")
+      .equalTo(areaId))
+      .snapshotChanges().subscribe(
+        (results: object) => { this.categories = results; }
+      );
 
     //Set the title
-    this.tabTitle = "Now, select a category...";
+    this.tabTitle = 'Category';
 
     //Set the tab to categories
     this.selectedIndex = 1;
+
+    this.catmodel.currentArea = areaId;
+
+    console.log(this.tabTitle);
+
   }
 
   //Function to call when a category is selected
-  loadSkills(categoryId) {
+  onCategorySelect(categoryId) {
 
     console.log(categoryId);
-    this.tabTitle = "Then select a Skill to Add!";
+
+    //Populate Categories - Firebase List w/ Sort&Filter Query
+    this.db.list('/skillcatalog/skills/', ref => ref
+      .orderByChild("category")
+      .equalTo(categoryId))
+      .snapshotChanges().subscribe(
+        (results: object) => { this.skills = results; }
+      );
+
+    this.tabTitle = "Skill";
     this.selectedIndex = 2;
+    this.catmodel.currentCategory = categoryId;
+  }
+
+  //Function to call when a category is selected
+  selectSkill(skillId) {
+    this.catmodel.currentSkill = skillId;
+  }
+
+  onAdd(): void {
+    console.log('add clicked');
+  }
+
+  onEdit(key): void {
+
+    // //Cast model to variable for formReset
+    // const mname: string = this.model.name;
+    // const mdescription: string = this.model.description;
+    // const mdatenow = Math.floor(Date.now());
+
+    // this.db.object('/users/' + this.fbuser.id + '/talents' + '/' + key)
+    //   .update({ name: mname, description: mdescription, created: mdatenow, modified: mdatenow, user: this.fbuser.id });
+    // this.db.object('/talents/' + this.fbuser.id + '/' + key)
+    //   .update({ name: mname, description: mdescription, created: mdatenow, modified: mdatenow, user: this.fbuser.id });
+    // this.showedititem = false;
+    console.log(key + ' edited');
+  }
+
+  onDelete(key): void {
+    // this.db.object('/users/' + this.fbuser.id + '/talents/' + key).remove();
+    // this.db.object('/talents/' + this.fbuser.id + '/' + key).remove();
+
+    // //Decrement Count
+    // this.db.object('/counts/' + this.fbuser.id + '/talents').query.ref.transaction(likes => {
+    //   if (likes === null) {
+    //     return likes = 0;
+    //   } else {
+    //     return likes - 1;
+    //   }
+    // })
+
+    console.log(key + ' deleted');
+
+  }
+
+  //Contextual Button based on tabTitle
+  onShowAddForm(type: string): void {
+    this.showedititem = false;
+    this.showadditem = true;
+    console.log(type + ' Form Open');
+  }
+
+  onHideAddForm(): void {
+    this.showadditem = false;
+  }
+
+  onShowEditForm(key): void {
+    console.log('edit form shown');
+    this.showadditem = false;
+    this.showedititem = true;
+
+    // //Define Observable
+    // this.item = this.db.object('/users/' + this.fbuser.id + '/talents/' + key).valueChanges();
+
+    // //Subscribe to Observable
+    // this.item.subscribe((item) => {
+    //   this.model = new Talent(key, item.name, item.description, item.created, item.modified, item.user);
+    // });
+
+    console.log(key + 'has been selected to edit');
+  }
+
+  onHideEditForm(): void {
+    this.showedititem = false;
+    this.model = new Skill('', '', '', '', '', '');
+  }
+
+  openConfirmationDialog(key): void {
+    // Open the dialog and save the reference of it
+    const dialogRef = this._fuseConfirmationService.open(this.dialogconfigForm.value);
+
+    // Subscribe to afterClosed from the dialog reference
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'confirmed') {
+        this.onDelete(key);
+      }
+    });
   }
 
   ngOnInit(): void {
 
-    //Populate the Areas Object
+    //Populate Areas - Firebase List Object
     this.areas = this.db.list('/skillcatalog/areas/').snapshotChanges();
 
-    //Populate Categories
-    this.db.list('/skillcatalog/categories/', ref => ref.orderByChild("area").equalTo(4)).snapshotChanges().subscribe(
-      (results: object) => {
-        console.log(results);
-        this.categories2 = results;
-      }
-    );
 
+    //Formbuilder for Dialog Popup
     this.dialogconfigForm = this._formBuilder.group({
       title: 'Remove Item',
-      message: 'Are you sure you want to remove this item permanently? <span class="font-medium">This action cannot be undone!</span>',
+      message: 'Are you sure you want to remove this ' + this.tabTitle + ' permanently? <span class="font-medium">This action cannot be undone!</span>',
       icon: this._formBuilder.group({
         show: true,
         name: 'heroicons_outline:exclamation',
@@ -132,3 +230,31 @@ export class MySkillsComponent implements OnInit {
   }
 
 }
+
+// Empty Skill class
+export class Skill {
+
+  constructor(
+    public key: string,
+    public name: string,
+    public description: string,
+    public created: string,
+    public modified: string,
+    public user: string,
+
+  ) { }
+
+}
+
+// Empty Skill class
+export class CatalogState {
+
+  constructor(
+    public currentArea: string,
+    public currentCategory: string,
+    public currentSkill: string,
+
+  ) { }
+
+}
+
