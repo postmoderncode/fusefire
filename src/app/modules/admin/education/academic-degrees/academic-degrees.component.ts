c522932c0124e874fb9ba7f0d43de1d334673c9d
bmlewandowski <bmlewandowski@gmail.com>
2022-06-19 15:48:05 -0400

    institution autocomplete finished

--- src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
+++ src/app/modules/admin/education/academic-degrees/academic-degrees.component.ts
@@ -39,7 +39,8 @@ export class AcademicDegreesComponent implements OnInit {
   //Autocomplete Data
   schoolfilteredData;
   fieldfilteredData;
-  schooloptions: string[] = ['Ball State University', 'Indiana University', 'Notre Dame', 'Purdue University', 'Indiana Tech'];
+  schooloptions;
+
   fieldoptions: string[] = ['Accounting', 'Advertising', 'African-American Studies', 'Agriculture', 'Animal Science', 'Anthropology', 'Aerospace Engineering', 'Archaeology', 'Architecture', 'Art History', 'Arts Management', 'Asian-American Studies', 'Astronomy and Astrophysics', 'Bilingual/Crosscultural Education', 'Athletic Training', 'Biochemistry', 'Biology', 'Biomedical Engineering', 'Business', 'Chemical Engineering', 'Chemistry', 'Civil Engineering', 'Classical Studies', 'Communication Disorders Sciences and Services', 'Communications', 'Comparative Literature', 'Computer Engineering', 'Computer Information Systems', 'Computer Science', 'Construction Services', 'Cosmetology Services', 'Creative Writing', 'Criminology', 'Culinary Arts', 'Cybersecurity', 'Design', 'Economics', 'Education', 'Electrical Engineering', 'Elementary Education', 'Engineering', 'English Language and Literature', 'Entomology', 'Environmental Engineering', 'Film and Video Production', 'Film-Video Arts', 'Finance', 'Fine Arts', 'Fire Safety Science', 'Food Science', 'Foreign Languages', 'Forestry', 'Gender Studies', 'Genetics', 'Geology', 'Graphic Design', 'Health Sciences', 'History', 'Hospitality Management', 'Human Ecology', 'Industrial Technology', 'International Business', 'International Relations', 'Journalism', 'Kinesiology', 'Latin American Studies', 'Liberal Studies', 'Library Science', 'Linguistics', 'Logistics Management', 'Marketing', 'Mathematics', 'Mechanical Engineering', 'Medical Technology', 'Metallurgical Engineering', 'Meteorology', 'Microbiology', 'Military Technology', 'Mining and Mineral Engineering', 'Music', 'Mythology and Folklore', 'Naval Architecture and Marine Engineering', 'Neuroscience', 'Nuclear Engineering', 'Nursing', 'Oceanography', 'Occupational Health and Safety', 'Parks, Recreation, and Leisure Studies', 'Performing Arts', 'Petroleum Engineering', 'Pharmacology', 'Philosophy', 'Photography', 'Physics', 'Physiology', 'Plant Science', 'Political Science', 'Pre-Law', 'Psychology', 'Public Administration', 'Puppetry', 'Religious Studies', 'Rhetoric', 'Social Work', 'Sociology', 'Software Engineering', 'Special Education', 'Sports Medicine', 'Statistics', 'Student Counseling', 'Supply Chain Management', 'Theater Arts', 'Viticulture', 'Zoology']
 
   //Dropdown List Data
@@ -87,6 +88,30 @@ export class AcademicDegreesComponent implements OnInit {
     private _fuseConfirmationService: FuseConfirmationService,
     public db: AngularFireDatabase
   ) { }
+
+  //Form Visibility Modifiers
+
+  onShowAddForm(): void {
+    this.showedititem = false;
+    this.showadditem = true;
+
+  }
+
+  onHideAddForm(): void {
+    this.showadditem = false;
+
+  }
+
+  onShowEditForm(key): void {
+    this.showadditem = false;
+    this.showedititem = true;
+
+  }
+
+  onHideEditForm(): void {
+    this.showedititem = false;
+
+  }
 
   onAdd(): void {
 
@@ -143,6 +168,19 @@ export class AcademicDegreesComponent implements OnInit {
 
   }
 
+  //Calendar Change Event to set Model
+  onDateChange(event: MatDatepickerInputEvent<any>, control: AbstractControl): void {
+
+    this.model.awardedon = ((event.value.valueOf()).toString());
+
+  }
+
+  //Degree Completed Checkbox
+  onCompletedChecked($event): void {
+    if ($event.checked === true) { this.gradDate = 'Date of Completion'; }
+    else { this.gradDate = 'Expected Graduation Date'; }
+  }
+
   //Filter for Field of Study Autocomplete
   applyFilterFields(evt: string): void {
     evt = evt + '';
@@ -157,57 +195,9 @@ export class AcademicDegreesComponent implements OnInit {
     evt = evt + '';
     if (!evt) { this.schoolfilteredData = this.schooloptions; }
     else {
-      this.schoolfilteredData = this.schooloptions.filter(item => (item + '') === evt || item.toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
+      this.schoolfilteredData = this.schooloptions.filter(item => (item.name + '') === evt || item.name.toString().toLocaleLowerCase().indexOf(evt.toLocaleLowerCase()) >= 0);
     }
   }
-
-  //Calendar Change Event to set Model
-  onDateChange(event: MatDatepickerInputEvent<any>, control: AbstractControl): void {
-
-    this.model.awardedon = ((event.value.valueOf()).toString());
-
-  }
-
-  //Degree Completed Checkbox
-  onCompletedChecked($event): void {
-    if ($event.checked === true) { this.gradDate = 'Date of Completion'; }
-    else { this.gradDate = 'Expected Graduation Date'; }
-  }
-
-
-  //Form Visibility Modifiers
-
-  onShowAddForm(): void {
-    this.showedititem = false;
-    this.showadditem = true;
-
-  }
-
-  onHideAddForm(): void {
-    this.showadditem = false;
-
-  }
-
-  onShowEditForm(key): void {
-    this.showadditem = false;
-    this.showedititem = true;
-
-  }
-
-  onHideEditForm(): void {
-    this.showedititem = false;
-
-  }
-
-  //State Dropdown Change Event
-  onStateChange(ob): void {
-
-    const selectedState = ob.value;
-    console.log(selectedState);
-
-    //TODO: Fill School Dropdown
-  }
-
 
   //Change fileter of degree types based on degree level dropdown
   onDegreeLevelChanged(ob): void {
@@ -215,6 +205,27 @@ export class AcademicDegreesComponent implements OnInit {
     this.degreetypesfilteredData = this.degreetypes.filter(degreetypes => degreetypes.degreeLevel === ob.value.toString());
 
   }
+
+  //State Dropdown Change Event
+  onStateChange(ob): void {
+
+    const selectedState = ob.value;
+
+    this.db.list('/institutions/', ref => ref
+      .orderByChild("state")
+      .equalTo(selectedState))
+      .valueChanges().subscribe(
+        (
+
+          results: object) => {
+          this.schooloptions = results;
+          this.schoolfilteredData = this.schooloptions.filter(institutions => institutions.state === selectedState);
+
+        }
+      );
+
+  }
+
 
   openConfirmationDialog(key): void {
     //Open the dialog and save the reference of it
@@ -263,10 +274,10 @@ export class AcademicDegreesComponent implements OnInit {
     //   }).unsubscribe;
 
     this.fieldfilteredData = this.fieldoptions;
+
+    //Only need if prepopulating dropbox without filter
     this.schoolfilteredData = this.schooloptions;
-    this.degreetypesfilteredData = this.degreetypes;
-
-
+    //this.degreetypesfilteredData = this.degreetypes;
 
 
   }
