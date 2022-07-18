9f4a08639596cb793ab67236ae38e9517353b2e6
bmlewandowski <bmlewandowski@gmail.com>
2022-07-18 02:57:10 -0400

    dashboard module fixes

--- src/app/modules/admin/home/dashboard/dashboard.module.ts
+++ src/app/modules/admin/home/dashboard/dashboard.module.ts
@@ -3,16 +3,8 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { Route, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatTabsModule } from '@angular/material/tabs';
-import { FuseAlertModule } from '@fuse/components/alert';
-import { SharedModule } from 'app/shared/shared.module';
+import { MatCardModule } from '@angular/material/card'
+import { FuseCardModule } from '@fuse/components/card';
 
 const dashboardRoutes: Route[] = [
   {
@@ -27,17 +19,9 @@ const dashboardRoutes: Route[] = [
   ],
   imports: [
     CommonModule,
-    MatButtonModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSlideToggleModule,
-    MatTabsModule,
-    FuseAlertModule,
-    SharedModule,
+    MatCardModule,
+    FuseCardModule,
     RouterModule.forChild(dashboardRoutes)
   ]
 })
