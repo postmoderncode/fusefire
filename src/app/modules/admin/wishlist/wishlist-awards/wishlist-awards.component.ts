e5fbbd09375d58417c94848258ef788726233f3f
bmlewandowski <bmlewandowski@gmail.com>
2022-08-02 00:11:10 -0400

    delete empty key checks

--- src/app/modules/admin/wishlist/wishlist-awards/wishlist-awards.component.ts
+++ src/app/modules/admin/wishlist/wishlist-awards/wishlist-awards.component.ts
@@ -238,34 +238,39 @@ export class WishlistAwardsComponent implements OnInit, OnDestroy {
   //Function - Delete Item in DB
   onDelete(key): void {
 
-    //Container for Strongly //Delete Item from the Item Node.
-    this.db.object('/wishlists/awards/' + key).remove().then((responseObject) => {
+    //Make sure empty key isn't passed to wipe database
+    if (key.length > 5) {
+
+      //Container for Strongly //Delete Item from the Item Node.
+      this.db.object('/wishlists/awards/' + key).remove().then((responseObject) => {
 
 
-      //Delete Item from the User Node.
-      this.db.object('/users/' + this.fbuser.id + '/wishlists/awards/' + key).remove().then((responseObject) => {
+        //Delete Item from the User Node.
+        this.db.object('/users/' + this.fbuser.id + '/wishlists/awards/' + key).remove().then((responseObject) => {
 
 
-        //Decrement Count
-        this.db.object('/counts/' + this.fbuser.id + '/wishlists/awards').query.ref.transaction((counts) => {
-          if (counts === null || counts <= 0) {
-            return counts = 0;
-          } else {
-            return counts - 1;
-          }
-        });
+          //Decrement Count
+          this.db.object('/counts/' + this.fbuser.id + '/wishlists/awards').query.ref.transaction((counts) => {
+            if (counts === null || counts <= 0) {
+              return counts = 0;
+            } else {
+              return counts - 1;
+            }
+          });
+
+        }
+        )
+
+          //Error Handling
+          .catch(errorObject => console.log(errorObject, 'Remove Item from the User Node Failed!'));
 
       }
       )
 
         //Error Handling
-        .catch(errorObject => console.log(errorObject, 'Remove Item from the User Node Failed!'));
+        .catch(errorObject => console.log(errorObject, 'Remove Item from the Item Node Failed!'));
 
     }
-    )
-
-      //Error Handling
-      .catch(errorObject => console.log(errorObject, 'Remove Item from the Item Node Failed!'));
 
     //Scroll to top
     this.cdkScrollable.scrollTo({ top: 0 });
